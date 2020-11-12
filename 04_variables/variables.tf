ebeaa78cbd66c7e973eaab79364f73e2580a8f06
ssachdev <sanjay.sachdev@hotmail.com>
2020-11-12 13:45:08 +0530

    Added variables for the credentials file and profile

--- 04_variables/variables.tf
+++ 04_variables/variables.tf
@@ -1,3 +1,13 @@
+variable "shared_credentials_file" {
+  type = string
+  default = "/home/ssachdev/.aws/credentials"
+}
+
+variable "profile" {
+  type = string
+  default = "lasandbox"
+}
+
 variable "vpn_ip" {
   default = "116.50.30.20/32"
 }
