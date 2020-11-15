ed113632ffd0a4e2760188afe49d9e1d17717227
Sanjay Sachdev <sanjaypsachdev@gmail.com>
2020-11-15 06:47:46 +0530

    Added code for pulling a module source from an existig github repo.

---
+++ 24_module_sources/module.tf
@@ -0,0 +1,5 @@
+module "demomodule" {
+  source = "git::https://github.com/zealvora/tmp-repo.git?ref=development"
+  #source = "github.com/zealvora/tmp-repo.git"
+  #source = "git::https://github.com/zealvora/tmp-repo.git"
+}
